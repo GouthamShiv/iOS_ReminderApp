bc8da69a0031c33c6eca4efef23cfbc4670c8810
Goutham S <gouthamshine@gmail.com>
2022-06-18 15:42:50 +0530

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Goutham on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
